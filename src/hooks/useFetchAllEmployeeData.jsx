d0d17694274ff8eeace096f4ea9283fe701585f6
Naveen <naveen.kaushik.21@mountblue.tech>
2024-03-13 11:58:59 +0530

    fetching data using axios

--- src/hooks/useFetchAllEmployeeData.jsx
+++ src/hooks/useFetchAllEmployeeData.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { GET_ALL_EMPLOYEE_DATA } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { setEmpData } from "../store/totalEmployeesDataSlice";
+import axios from "axios";
 
 const useFetchAllEmployeeData = () => {
   const dispatch = useDispatch();
@@ -11,11 +12,18 @@ const useFetchAllEmployeeData = () => {
   }, []);
 
   // http://localhost:8080/employee/all
-  const fetchData = async () => {
-    const data = await fetch(GET_ALL_EMPLOYEE_DATA);
-    const json = await data.json();
-    console.log(json);
-    dispatch(setEmpData(json));
+  // const fetchData = async () => {
+  //   const data = await fetch(GET_ALL_EMPLOYEE_DATA);
+  //   const json = await data.json();
+  //   console.log(json);
+  //   dispatch(setEmpData(json));
+  // };
+
+  const fetchData = () => {
+    const data = axios(GET_ALL_EMPLOYEE_DATA).then((res) =>
+      dispatch(setEmpData(res.data))
+    );
+    console.log(data);
   };
   return <div></div>;
 };
